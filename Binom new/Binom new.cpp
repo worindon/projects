73b0da98bab9bdc71914b7247825825ad8c54f79
Ярослав Руссу <russu636@gmail.com>
2024-02-22 19:55:11 +0200

    \\

--- Binom new/Binom new.cpp
+++ Binom new/Binom new.cpp
@@ -32,57 +32,57 @@ void Comb_nums(int* nums, int n, int x, int y) {
         nums[i] = comb(n, i) * pow(x, n - i) * pow(y, i);
 
     }
-    
+
 }
 
 void print_pre(int x, int y, int n) {
-   
-
-        cout << "( ";
-
-   
-
-        if (x<0) {
-
-            cout << "- " << -x << "a";
-
-        }
-
-        else if (x > 0) {
-            if (x > 1) cout << x;
-            cout << "a";
-
-        }
-
-       
-        
-
-        if (y<0) {
-
-          cout << " - " << -y << "b";
-        }
-
-        else if(y>0) {
-
-            if (x)  cout << " + ";
-
-            if (y > 1) cout << y;
-
-            cout <<  "b";
-
-        }
 
 
-        if (!x and !y) {
-            cout << "0 + 0";
-            
-        }
+    cout << "(";
 
-  
 
-        cout << " ) ^ "<<n;    
 
-            cout << " = ";
+    if (x < 0) {
+
+        cout << "- " << -x << "a";
+
+    }
+
+    else if (x > 0) {
+        if (x > 1) cout << x;
+        cout << "a";
+
+    }
+
+
+
+
+    if (y < 0) {
+
+        cout << " - " << -y << "b";
+    }
+
+    else if (y > 0) {
+
+        if (x)  cout << " + ";
+
+        if (y > 1) cout << y;
+
+        cout << "b";
+
+    }
+
+
+    if (!x and !y) {
+        cout << "0 + 0";
+
+    }
+
+
+
+    cout << ") ^ " << n;
+
+    cout << " = ";
 
     if (!n) {
         cout << " 1 ";
@@ -97,25 +97,26 @@ void print_pre(int x, int y, int n) {
 
 void print_res(int x, int y, int n, int* nums) {
 
-    for (int i = 0; i <= n; i++) {
+    if (x or y) {
+        for (int i = 0; i <= n; i++) {
 
 
-        if (i and nums[i]>0) cout << " + ";
+            if (i and nums[i] > 0) cout << " + ";
 
-        if (nums[i] > 1) cout << nums[i];
-         else
-            cout << " - " << nums[i] * (-1);
+            if (nums[i] > 1) cout << nums[i];
+            else
+                cout << " - " << nums[i] * (-1);
 
-        if (i > 1) cout << "(a)^" << i;
+            if (i > 1) cout << "(a)^" << i;
 
             else if (i == 1) cout << "(a)";
 
-        if ((n - i) > 1) cout << "(b)^" << n - i;
+            if ((n - i) > 1) cout << "(b)^" << n - i;
 
             else if ((n - i) == 1) cout << "(b)";
+        }
+
     }
-
-
 
 
 }
@@ -125,27 +126,28 @@ void print_res(int x, int y, int n, int* nums) {
 int main()
 {
 
-    int x, y, n;   
-    
+    int x, y, n;
+
 
     cout << endl << "\t " << "In this aplication Newton`s binom present as\t(xa + yb) ^ n" <<
-        endl<<" \t n must be > or = 0" << endl << endl << "\t" << " Write your \"x\"\t";  cin >> x;
-        
-        cout << endl << "\t" << " Write your \"y\"\t";      cin >> y;
-        
-            cout << endl << "\t" << " Write your \"n\"\t";      cin >> n;
-                
-                cout << endl;
-                    
-                    cout << endl << "\t" << "It`s -- \t ";
+        endl << " \t n must be > or = 0" << endl << endl << "\t" << " Write your \"x\"\t";  cin >> x;
+
+    cout << endl << "\t" << " Write your \"y\"\t";      cin >> y;
+
+    cout << endl << "\t" << " Write your \"n\"\t";      cin >> n;
+
+    cout << endl;
+
+    cout << endl << "\t" << "It`s -- \t ";
 
 
-   print_pre( x, y, n); 
-   
+    print_pre(x, y, n);
+
     if (n > 0) {
         int* nums = new int[n];
         Comb_nums(nums, n, x, y);
         print_res(x, y, n, nums);
     }
+    cout << endl << endl << endl;
     system("pause");
 }
