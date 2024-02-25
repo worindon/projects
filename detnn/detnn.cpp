dbcc8f08a5c90c6c2c19822387a07ff353b417ce
Ярослав Руссу <russu636@gmail.com>
2024-02-25 23:04:09 +0200

    добавлены новые функции для чека треугольного вида, наличия нуля на гл. диагонали  а также: непосредственный ввод/вывод матрицы, функция очистки консоли, умножение с возвратом новой матрицы, свапер рядков(ручной режим)

--- detnn/detnn.cpp
+++ detnn/detnn.cpp
@@ -5,36 +5,55 @@ using namespace std;
 
 
 
+double detetrminant = 1.0;
+int paramsize;
+
 
 
 void det(vector<vector<double>>& matrix) {
 
-	for (int r = 0; r < matrix.size(); r++) {
+	int size = matrix.size(); 
 
-		for (int i = r+1; i < matrix.size(); i++) {
+	for (int r = 0; r < size; r++) {
+
+		for (int i = r+1; i < size; i++) {
 
 			double k = matrix[i][r] / matrix[r][r];
 
-			for (int j = r; j < matrix.size(); j++) {
+			for (int j = r; j < size; j++) {
 
 				matrix[i][j] -= matrix[r][j] * k;
 				
 			}			
 		}
 	}	
+
+	for (int i = 0; i < size; i++) {
+
+		detetrminant *= matrix[i][i];
+
+	}
 }
 
 
-bool qwest() {
+vector <vector <double>>  multiplication_matrix(vector<vector <double>>& matrix1, vector<vector <double>>& matrix2) {
 
-	char i;
-	do {
-		scanf_s("%c", &i, sizeof(i));
-	} while (!(i == 'Y' || i == 'N' || i == 'y' || i == 'n'));
+	int n = matrix1.size();
+	vector<vector <double>> resmatrix(n, vector <double>(n));
 
-	if (i == 'y' || i == 'Y') return 1;
-	return 0;
+	for (int i = 0; i < n; i++) {
 
+		for (int j = 0; j < n; j++) {
+
+			for (int k = 0; k < n; k++) {
+
+				resmatrix[i][j] += matrix1[i][k] * matrix2[k][j];
+			}
+
+		}
+	}
+
+	return resmatrix;
 }
 
 
@@ -64,62 +83,101 @@ void print_matrix(vector<vector <double>> matrix) {
 }
 
 
-
 void cons_clear() {
 	cout << "\033[2J\033[1;1H";
 
 }
 
+
+bool qwest() {
+
+	cout << endl;
+	char i;
+	do {
+		scanf_s("%c", &i, sizeof(i));
+	} while (!(i == 'Y' || i == 'N' || i == 'y' || i == 'n'));
+
+	if (i == 'y' || i == 'Y') return 1;
+	return 0;
+
+}
+
+
+bool matrix_triangle_check(vector<vector <double>>& matrix) {
+
+	double k = matrix.size();
+
+	for (int i = 1; i < k; i++) {
+
+		for (int j = 0; j <= i; j++) {
+
+			if (matrix[i][j]) return 1;
+		
+		}
+	}
+
+	return 0;
+}
+
+
+int matrix_diagonal_check(vector<vector <double>>& matrix) {
+
+	for (int i = 0; i < matrix.size(); i++) {
+
+		if (!(matrix[i][i])) return i;
+
+	}
+}
+
+
+void matrix_line_swap(vector<vector <double>>& matrix) {
+
+	int a, b, size = matrix.size();
+
+	cout << "Message";
+
+	do {
+		cin >> a >> b;
+
+	} while (!(a < size && b < size));
+
+	a--; b--;
+
+		for (int i = 0; i < matrix.size(); i++) {
+		
+			swap(matrix[i][a], matrix[i][b]);
+
+		}	
+
+detetrminant *= -1;
+
+}
+
+
 int main(){
 
-
-
-	cout << "Write size of your square matrix and them write your numbers" << endl;
-	int n;
-	cin >> n;
-	vector < vector <double> > a(n, vector <double>(n));
+	setlocale(LC_ALL, "ru");
+	cin >> paramsize;
 
 
 
-	input_matrix(a);
-
-	cout << endl << endl << endl << endl;
+	//cout << "fefe" << endl;
+	vector < vector <double> > matrix1(paramsize, vector <double>(paramsize));
 	
-	print_matrix(a);
+	input_matrix(matrix1);
 
-	cout << endl << endl << endl << endl;
 	
-	det(a);
+
+
+
+// = multiplication_matrix(a1, a2);
+//vector < vector <double> > a1(n, vector <double>(n));
 	
-	
-	for (int i = 0; i < n; i++)
-	{
-		cout << "\t";
-		for (int j = 0; j < n; j++)
-		{
-			cout << a[i][j] << " ";
-		}
-		cout << endl;
-	}
-	cout << endl;
 
-	long  long res = 1;
-	for (int i = 0, j = 0; i < n; i++, j++) {
-		res *= a[i][j];
 
-	}
 
-	cout << "det " << res << " | ";
 
-	for (int i = 0, j = 0; i < n; i++, j++)
-	{
-		cout << a[i][j];
-		if (j < n - 1) {
-			cout << " * ";
-		}
-	}
-	
-	cout << endl;
+
 	system("pause");
 }
 
