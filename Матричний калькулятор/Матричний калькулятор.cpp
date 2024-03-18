b62381c7630c26f61fc2962cdab27b10d601c31d
Ярослав Руссу <russu636@gmail.com>
2024-03-18 10:34:44 +0200

    //

--- Матричний калькулятор/Матричний калькулятор.cpp
+++ Матричний калькулятор/Матричний калькулятор.cpp
@@ -1,7 +1,7 @@
 ﻿#include <windows.h>
 #include "Actions.h"
 int main() {
-
+    system("chcp 1251");
     SetConsoleTitle(L"Матричный калькулятор");
 
     // Получаем дескриптор консольного окна
@@ -16,7 +16,7 @@ int main() {
     MoveWindow(consoleWindow, rect.left, rect.top, 1300, 600, TRUE);    
     SetWindowLong(consoleWindow, GWL_STYLE, GetWindowLong(consoleWindow, GWL_STYLE) & ~WS_SIZEBOX);
 
-    setlocale(LC_ALL, "ru");
+    //setlocale(LC_ALL, "ru");
     Matrix A(6, 6);
     Matrix B(6, 6);
     while(!main_menu(A, B)){}
